a47ebfb0fcad964db1cb9fc3a32c09e609ba3331
Jakub Brehuv <kubek.breha@gmail.com>
2018-07-29 21:27:31 +0200

    reduce size and repositioning

--- BuildStack/Assets/Scripts/TheStack.cs
+++ BuildStack/Assets/Scripts/TheStack.cs
@@ -6,16 +6,22 @@ public class TheStack : MonoBehaviour {
 
     private const float STACK_MOVING_SPEED = 5.0f;
     private const float BOUNDS_SIZE = 3.8f;
+    private const float ERROR_MARGIN = 0.1f;
+
 	private GameObject[] theStack;
+    private Vector2 stacksBounds = new Vector2(BOUNDS_SIZE, BOUNDS_SIZE);   
 
     private int stackIndex;
     private int scoreCount = 0;
+    private int combo = 0;
 
     private float tileTransaction = 0.0f;
     private float tileSpeed = 2.0f;
     private float secondaryPossition;
 
     private Vector3 desiredPosition;
+    private Vector3 lastTilePossition;
+
     private bool isMovingOnX = true;
 
 	private void Start () {
@@ -58,6 +64,7 @@ public class TheStack : MonoBehaviour {
 
 
     private void SpawnTile(){
+        lastTilePossition = theStack[stackIndex].transform.localPosition;
         stackIndex--;
         if(stackIndex < 0){
             stackIndex = transform.childCount - 1;
@@ -70,6 +77,40 @@ public class TheStack : MonoBehaviour {
     private bool PlaceTile()
     {
         Transform t = theStack[stackIndex].transform;
+
+        if(isMovingOnX){
+            float deltaX = lastTilePossition.x - t.position.x;
+            if(Mathf.Abs (deltaX) > ERROR_MARGIN){
+
+                //Cuting the tile
+                combo = 0;
+                stacksBounds.x -= Mathf.Abs(deltaX);
+                if(stacksBounds.x < 0){
+                    return false;
+                }
+
+                float middle = lastTilePossition.x + t.localPosition.x / 2;
+                t.localScale = new Vector3(stacksBounds.x, 1, stacksBounds.y);
+                t.localPosition = new Vector3(middle - (lastTilePossition.x / 2), scoreCount, lastTilePossition.z);
+            }
+        }else{
+            float deltaZ = lastTilePossition.z - t.position.z;
+            if (Mathf.Abs(deltaZ) > ERROR_MARGIN)
+            {
+
+                //Cuting the tile
+                combo = 0;
+                stacksBounds.y -= Mathf.Abs(deltaZ);
+                if (stacksBounds.y < 0)
+                {
+                    return false;
+                }
+
+                float middle = lastTilePossition.z + t.localPosition.z / 2;
+                t.localScale = new Vector3(stacksBounds.x, 1, stacksBounds.y);
+                t.localPosition = new Vector3(lastTilePossition.x, scoreCount, middle - (lastTilePossition.z / 2));
+            }
+        }
 
         secondaryPossition = isMovingOnX ? t.localPosition.x : t.localPosition.z;
 
